48143df3378d194cbe296a0544dc47a0037e864a
wangrui <worrylive@qq.com>
2020-07-03 17:18:49 +0800

    建言献策模块PC端发帖和通知页面； 修改建言献策项目使支持纯静态资源引用； 富文本控件使用自托管，支持自定义皮肤和语言；

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,18 @@
 import Layout from '@page/layout.vue'
+import Compose from '@page/compose.vue'
+import Notice from '@page/notice.vue'
 const forumLayout = {
   path: '/mfs-forum',
-  component: Layout
+  component: Layout,
+  children: [{
+    name: 'compose',
+    path: 'compose',
+    component: Compose
+  }, {
+    name: 'notice',
+    path: 'notice',
+    component: Notice
+  }]
 }
 
 export default [forumLayout]
