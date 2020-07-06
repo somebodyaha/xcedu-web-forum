d9c8f1656fd4896d4f9504f35d5b9223c197a893
xzl <1370914609@qq.com>
2020-07-06 16:46:56 +0800

    xzl

--- src/pages/manage/super-manage.vue
+++ src/pages/manage/super-manage.vue
@@ -1,6 +1,13 @@
 <template>
-  <section>
-    <manage />
+  <section class="superManage">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="内容管理" name="first">
+        <manage />
+      </el-tab-pane>
+      <el-tab-pane label="板块管理" name="second">
+        <manage />
+      </el-tab-pane>
+    </el-tabs>
   </section>
 </template>
 
@@ -9,6 +16,17 @@ import manage from '@page/components/posts-manage'
 export default {
   components: {
     manage
+  },
+  data () {
+    return {
+      activeName: 'first'
+    }
+  },
+  methods: {
+    handleClick (tab, event) {
+      // eslint-disable-next-line no-console
+      console.log(tab, event.target)
+    }
   }
 }
 </script>
