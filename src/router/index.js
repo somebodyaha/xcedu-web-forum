2c199ab4c04292030f457c63e91f28c0f1a1445d
wangrui <worrylive@qq.com>
2020-07-21 21:13:11 +0800

    附件上传

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import routes from './routes'
 
 // 由于Vue Vue-router 是 global import， vue-roter 会自动调用  vue-router的install方法， 所以不再需要手动use
-Vue.use(VueRouter)
+// Vue.use(VueRouter)
 
 export default new VueRouter({
   mode: 'history',
