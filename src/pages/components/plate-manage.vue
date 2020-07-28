2ceb8210b1a2508a9f7968abf4f24076926a041e
JiangZuoMeiLangZhe <1311198116@qq.com>
2020-07-28 17:56:04 +0800

    修改bug

--- src/pages/components/plate-manage.vue
+++ src/pages/components/plate-manage.vue
@@ -156,7 +156,7 @@ export default {
     },
     choose (title, row) {
       if (title === 'del') {
-        this.$confirm('此操作将删除该帖子 , 是否继续?', '提示', {
+        this.$confirm('此操作将删除该版块 , 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
